b76761dabfa7f73da72fa05dbc51d3c4e2380e1a
AhmadWaldiArridho <AhmadWaldiArridho>
2022-11-06 12:49:16 +0700

    commit pertama

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/index")
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(router)
+
+
+app.listen(PORT, () => {
+    console.info(`Running at port http://localhost:${PORT}`);
+})

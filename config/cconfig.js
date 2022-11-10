af5018959b5cd10e9c94ee5e9628b76a6357adb9
AhmadWaldiArridho <AhmadWaldiArridho>
2022-11-10 08:48:31 +0700

    deploy

---
+++ config/cconfig.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+
+module.exports = {
+
+    "development": {
+      "username": "ahmad",
+      "password": "password",
+      "database": "db_tokoBelanja",
+      "host": "127.0.0.1",
+      "dialect": "postgres"
+    },
+    "test": {
+      "username": "root",
+      "password": null,
+      "database": "database_test",
+      "host": "127.0.0.1",
+      "dialect": "mysql"
+    },
+    "production": {
+      "username": process.env.PGUSER,
+      "password": process.env.PGPASSWORD,
+      "database": process.env.PGDATABASE,
+      "host": process.env.PGHOST,
+      "dialect": "postgres"
+    }
+  }
